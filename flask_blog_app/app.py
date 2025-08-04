71edf058229a7681a6166931e018d046c2f9f6ca
arav20603 <dakshandaravind28@gmail.com>
2025-08-04 10:59:04 +0530

    Created update, select and delete funtionalities using json body

--- flask_blog_app/app.py
+++ flask_blog_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
@@ -19,14 +19,64 @@ def hello():
 def store_data():
     try:
         cur = mysql.connection.cursor()
-        title = request.form['title']
-        content = request.form['content']
+        # title = request.form['title']
+        # content = request.form['content']
+        json = request.get_json()
+        title = json.get('title')
+        content = json.get('content')
         sql = 'insert into blog (title, content) values (%s, %s)'
         val = (title, content)
         cur.execute(sql, val)
         mysql.connection.commit()
         cur.close()
-        return 'success'
+        return f'Inserted data successfully: \nTitle: {title}\nContent: {content}'
+    except Exception as e:
+        return f'Error in inserting data: {e}'
+    
+@app.route('/update', methods=['post'])
+def updateData():
+    try:
+        cur = mysql.connection.cursor()
+        # title = request.form['title']
+        # content = request.form['content']
+        json = request.get_json()
+        id = json.get('id')
+        title = json.get('title')
+        content = json.get('content')
+        sql = "update blog set title = %s, content = %s where id = %s"
+        val = [title, content, id]
+        cur.execute(sql, val)
+        mysql.connection.commit()
+        cur.close()
+        return f'Updated data successfully:\nId: {id}\nTitle: {title}\nContent: {content}'
+    except Exception as e:
+        return f'Error is updating data: {e}'
+
+@app.route('/delete', methods=['post'])
+def deleteData():
+    try:
+        cur = mysql.connection.cursor()
+        json = request.get_json()
+        id = json.get('id')
+        cur.execute(f'Select * from blog where id = {id}')
+        res = cur.fetchall()
+        sql = f'Delete from blog where id = {id}'
+        cur.execute(sql)
+        mysql.connection.commit()
+        cur.close()
+        return f'Deleted data successfully:\nId: {res[0][0]}\nTitle: {res[0][1]}\nContent: {res[0][2]}'
+    except Exception as e:
+        return f"Error in deleting data: {e}"
+    
+@app.route('/get')
+def getData():
+    try:
+        cur = mysql.connection.cursor()
+        sql = 'select * from blog'
+        cur.execute(sql)
+        res = cur.fetchall()
+        cur.close()
+        return jsonify(res)
     except Exception as e:
         return f'Error in inserting data: {e}'
 
